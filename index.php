9af27700b7abaed2f78c1429f7d39d663c09f650
Oliver Crowe <olivercampbellcrowe@outlook.com>
2019-08-14 22:36:56 +0100

    Initial files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link href="https://fonts.googleapis.com/css?family=Blinker&display=swap" rel="stylesheet"> 
+    <title>StrippedDiary, create an account and begin writing</title>
+    <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <div class="txt-box">
+            <h1 class="heading">STRIPPED DIARY</h1>
+            <p class="sub-heading">A visually stripped diary</p>
+        </div>
+    </header>
+    </body>
+</html>

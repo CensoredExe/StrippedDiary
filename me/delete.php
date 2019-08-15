be5a103b0ee3d208a3ed726425267e2a3fa44fab
Oliver Crowe <olivercampbellcrowe@outlook.com>
2019-08-15 04:32:11 +0100

    Delete functionality + Deleted table in DB

--- me/delete.php
+++ me/delete.php
@@ -3,7 +3,34 @@
         session_start();
         include_once "../includes/include.php";
         $id = mysqli_real_escape_string($conn, $_GET['id']);
-        
+        $user_id = $_SESSION['user_id'];
+        $sql = "SELECT * FROM posts WHERE post_id='$id' AND post_author='$user_id'";
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) == 1){
+            while($row = mysqli_fetch_assoc($result)){
+                $post_title = $row['post_title'];
+                $post_date = $row['post_date'];
+                $post_content = $row['post_content'];
+                $post_id = $row['post_id'];
+                $post_author = $row['post_author'];
+
+                $sqladd = "INSERT INTO deleted (`post_title`, `post_date`, `post_content`, `post_id`, `post_author`) 
+                VALUES ('$post_title', '$post_date', '$post_content', '$post_id', '$post_author');";
+                if(mysqli_query($conn, $sqladd)){
+                    $sql = "DELETE FROM posts WHERE post_id='$post_id'";
+                    if(mysqli_query($conn, $sql)){
+                        echo "<script>window.location = 'index.php'</script>";
+                    }else {
+                        echo "Error 2";
+                    }
+                }else {
+                    echo "Error 1";
+                }
+            }
+        }else {
+            echo "Post doesnt exist or you dont own it.";
+            exit();
+        }
     }else {
         echo "<script>window.location = 'index.php'</script>";
     }
